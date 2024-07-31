e450d822f6a37ad9607feb0adb856930a2a2b135
rmaurya <rahulmaurya1036@gmail.com>
2024-07-31 19:21:06 +0530

    add: added github pages support

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test Rahul's Project
+          Edit <code>src/App.jsx</code> and save to test The Rahul's Project
         </p>
       </div>
       <p className="read-the-docs">
