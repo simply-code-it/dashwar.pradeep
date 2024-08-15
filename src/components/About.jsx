3be1f478dc068887921506f5126bf4a7db57c65f
rmaurya <rahulmaurya1036@gmail.com>
2024-08-15 14:57:32 +0530

    added : responsiveness added about section and also i was added  previous commit toggle button in project components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,133 +17,108 @@ const About = () => {
   const bgColor = colorMode === "dark" ? "#2c2c2c" : "#cecece";
   const highlightColor = colorMode === "dark" ? "#7b66ff" : "#4a4a4a";
   const textColor = colorMode === "dark" ? "#ffffff" : "#000000";
-
-  // Use useColorModeValue correctly
   const boxBgColor = useColorModeValue("white", "#292929");
 
   return (
     <section id="about">
       <Box bgColor={bgColor} pb={100}>
-        {/* image section */}
+        {/* Image Section */}
         <Box pt={10} display="flex" justifyContent="center" alignItems="center">
-          <Image borderRadius="15px" boxSize="300px" src={pic1} alt="rahul" />
+          <Image
+            borderRadius="15px"
+            boxSize={{ base: "200px", md: "250px", lg: "300px" }}
+            src={pic1}
+            alt="rahul"
+          />
         </Box>
-        <Box textAlign={"center"} maxW={"80%"} margin={"auto"} pt={10}>
+
+        <Box textAlign="center" maxW="80%" margin="auto" pt={10}>
           <Text
             fontSize={{ base: "2xl", md: "4xl", lg: "5xl" }}
-            fontWeight={"bold"}
-            textShadow={"0 0 15px #7b66ff"}
+            fontWeight="bold"
+            textShadow="0 0 15px #7b66ff"
           >
             ABOUT ME
           </Text>
           <Box
             h={1}
-            w={"30%"}
-            margin={"auto"}
-            bg={"#7b66ff"}
-            borderRadius={"full"}
+            w="30%"
+            margin="auto"
+            bg="#7b66ff"
+            borderRadius="full"
             mb={5}
           ></Box>
-          <Text fontSize={{ sm: "l", md: "l", lg: "xl" }}>
+          <Text fontSize={{ base: "md", md: "lg", lg: "xl" }}>
             Here you will find more information about me, what I do, and my
             current skills mostly in terms of programming and technology.
           </Text>
         </Box>
-        <Box maxW={"50%"} margin={"auto"} mt={5} textAlign={"center"}>
-          <Text fontSize={"2xl"} fontWeight={"bold"}>
+
+        <Box
+          maxW={{ base: "90%", md: "50%" }}
+          margin="auto"
+          mt={5}
+          textAlign="center"
+        >
+          <Text fontSize={{ base: "xl", md: "2xl" }} fontWeight="bold">
             Get to know me!
           </Text>
-          <Text fontSize={{ sm: "l", md: "l", lg: "xl" }}>
+          <Text fontSize={{ base: "md", md: "lg", lg: "xl" }}>
             I'm a Fullstack Web Developer building both the front end and back
             end of Websites and Web Applications that contribute to the success
             of the overall product. Check out some of my work in the Projects
             section.
           </Text>
-          <Text fontSize={{ sm: "l", md: "l", lg: "xl" }}>
+          <Text fontSize={{ base: "md", md: "lg", lg: "xl" }}>
             I'm open to job opportunities where I can contribute, learn, and
             grow. If you have a good opportunity that matches my skills and
             experience, don't hesitate to contact me.
           </Text>
 
+          {/* Contact Detail Section */}
           <VStack
             mt={5}
-            p={6}
+            p={10}
             rounded="lg"
             bg={boxBgColor}
             boxShadow="lg"
-            maxW="100%"
-            textAlign="left"
-            // justify={center}
-            // justify="center"
-
+            maxW={{ base: "90%", md: "50%", lg: "300px" }} // Adjusted for centering
+            margin="auto" // Center the box
+            textAlign="center"
+            spacing={4}
           >
-            <Flex  >
-              <Text
-                fontSize="xl"
-                fontWeight="bold"
-                color={highlightColor}
-                ml={2}
-                minW="180px"
-                
-              >
-                Name :
+            <Flex flexDirection="column" align="center" w="100%" mb={3}>
+              <Text fontSize="xl" fontWeight="bold" color={highlightColor}>
+                Name:
               </Text>
-              <Text
-                ml={100}
-                fontSize="lg"
-                fontWeight="semibold"
-                color={textColor}
-              >
+              <Text fontSize="lg" fontWeight="semibold" color={textColor} mt={1}>
                 Rahul Maurya
               </Text>
             </Flex>
 
-            <Flex >
-              <Text
-                fontSize="xl"
-                fontWeight="bold"
-                color={highlightColor}
-                mb={1}
-                minW="150px"
-              >
-                Mobile Number :
+            <Flex flexDirection="column" align="center" w="100%" mb={3}>
+              <Text fontSize="xl" fontWeight="bold" color={highlightColor}>
+                Mobile Number:
               </Text>
-              <Text
-                ml={100}
-                fontSize="lg"
-                fontWeight="semibold"
-                color={textColor}
-              >
+              <Text fontSize="lg" fontWeight="semibold" color={textColor} mt={1}>
                 +91 7011101778
               </Text>
             </Flex>
 
-            <Flex  >
-              <Text
-                fontSize="xl"
-                fontWeight="bold"
-                color={highlightColor}
-                ml={5}
-                // minW="150px"
-              >
-                E-Mail :
+            <Flex flexDirection="column" align="center" w="100%">
+              <Text fontSize="xl" fontWeight="bold" color={highlightColor}>
+                E-Mail:
               </Text>
-              <Text
-                ml={100}
-                fontSize="lg"
-                fontWeight="semibold"
-                color={textColor}
-              >
+              <Text fontSize="lg" fontWeight="semibold" color={textColor} mt={1}>
                 rahulmaurya1036@gmail.com
               </Text>
             </Flex>
-            
           </VStack>
 
           <AnchorLink href="#contact">
             <Button
               mt={8}
-              bg={"#7B66FF"}
+              bg="#7B66FF"
               _hover={{
                 transform: "scale(1.1)",
                 boxShadow: "0 0 10px gray",
